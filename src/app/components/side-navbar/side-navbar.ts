d1de38d4f3bd39749a4d17b28982b55b80534a39
Harish <harishsomsole@gmail.com>
2025-09-07 12:14:57 +0530

    created components user, side_navbar

---
+++ src/app/components/side-navbar/side-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navbar',
+  imports: [],
+  templateUrl: './side-navbar.html',
+  styleUrl: './side-navbar.css'
+})
+export class SideNavbar {
+
+}
