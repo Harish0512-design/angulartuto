d86728bb6642d8e135769265f14ad0a9131a7375
Harish <harishsomsole@gmail.com>
2025-09-07 13:06:36 +0530

    life cycle events

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
+export class User implements OnInit, AfterViewInit, OnDestroy {
+  constructor() {
+    console.log('User component initialized');
+    // property initialization or other setup can go here
+  }
+
+  ngOnInit() {
+    console.log('User component loaded');
+    // api call
+    // subscription
+  }
+
+  ngAfterViewInit() {
+    console.log('User component view initialized');
+    // DOM manipulations
+    // executes after all the component views are initialized
+  }
+
+  ngOnDestroy(): void {
+    console.log('User component destroyed');
+    // cleanup tasks
+    // unsubscribe from observables
+    // detach event handlers to avoid memory leaks
+    // leaving / navigating away from the component
+  }
 
 }
