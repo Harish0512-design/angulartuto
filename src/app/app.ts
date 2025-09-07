f2feed44e6e6a69a1aed736df22e5ef5a2285009
Harish <harishsomsole@gmail.com>
2025-09-07 15:45:50 +0530

    clean up previous components to create a ems portal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideNavbar } from "./components/side-navbar/side-navbar";
-import { User } from "./components/user/user";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideNavbar, User],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
