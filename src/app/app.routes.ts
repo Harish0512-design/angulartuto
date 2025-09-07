ef596f31d693b834e4795a4f3bd39eda7b3b2907
Harish <harishsomsole@gmail.com>
2025-09-07 15:55:21 +0530

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Layout } from './components/layout/layout';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Employee } from './components/employee/employee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: "employee",
+                component: Employee
+            }
+        ]
+    }
+];
